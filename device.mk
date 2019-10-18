473663d429f32c49d592a4885ba825f2f9525c37
Igor Murashkin <iam@google.com>
2019-10-18 11:33:12 -0700

    mido: Enable iorapd tracing with perfetto
    App startups will now have perfetto-based tracing from iorapd.
    
    Bug: 141378186
    Test: reflash and check getprop
    Change-Id: Icde1e38f59c9d1d0632a0ea424a498c1f2d163e2

--- device.mk
+++ device.mk
@@ -258,6 +258,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
+# Iorapd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.iorapd.enable=true \
+    iorapd.perfetto.enable=true
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
