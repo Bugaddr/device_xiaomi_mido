af72efd9debee424501f12ab4be2899c09e15c12
Erickeagle3 <sayanchakraborty334@gmail.com>
2021-05-29 07:08:08 +0000

    mido: enable updateable apex

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Sna
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
