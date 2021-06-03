57ee36385007d10fffb45255b2a89e92c2e43022
JR CHINTU <harrypetersun@outlook.com>
2021-06-03 13:39:55 +0000

    [squash] Add Florisboard
    Add Remove Packages
    
    Signed-off-by: JR CHINTU <harrypetersun@outlook.com>
    
    Add FlorisBoard Keyboard BY default
    
    Signed-off-by: JR CHINTU <harrypetersun@outlook.com>
    
    Add flag to make florisboard
    
    Signed-off-by: JR CHINTU <harrypetersun@outlook.com>

--- device.mk
+++ device.mk
@@ -429,3 +429,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# FlorisBoard Keyboard
+PRODUCT_PACKAGES += \
+    Florisboard
--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := Email Exchange Exchange2 UnifiedEmail PhotoTable Eleven
+LOCAL_OVERRIDES_PACKAGES := Email Exchange Exchange2 UnifiedEmail Eleven messaging ExactCalculator Calendar Terminal Gallery2 Contacts LatinIME libjni_latinimegoogle DeskClock AEXPapers XiaomiParts WallpaperPickerGoogle OpenCamera WallpaperPicker2 Camera2 FM2 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
