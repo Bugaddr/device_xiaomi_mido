7ddcc025e74dc3451179e446e857ce2b35509ab1
JR CHINTU <harrypetersun@outlook.com>
2021-06-07 11:32:36 +0000

    Add New apps to remove_packages
    Signed-off-by: JR CHINTU <harrypetersun@outlook.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := Email Exchange Exchange2 UnifiedEmail Eleven messaging ExactCalculator Calendar Terminal Gallery2 Contacts LatinIME libjni_latinimegoogle DeskClock AEXPapers XiaomiParts WallpaperPickerGoogle OpenCamera WallpaperPicker2 Camera2 FM2 
+LOCAL_OVERRIDES_PACKAGES := ExactCalculator Calendar CalenderProvider Gallery Gallery2 LatinIME libjni_latinimegoogle DeskClock AEXPapers LiveWallpapersPicker FM2 Music EasterEgg BasicDreams BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicPlayerGO MusicFX PhotoTable Stk StorageManager Traceur Terminal
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
