d10e1d2b36e41af72e03f2bc2e5501420e14695f
CHINTU <harrypetersun@outlook.com>
2021-06-08 19:05:50 +0530

    Add ddg wallpicker bips

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := ExactCalculator Calendar CalenderProvider Gallery Gallery2 LatinIME libjni_latinimegoogle DeskClock AEXPapers LiveWallpapersPicker FM2 Music EasterEgg BasicDreams BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicPlayerGO MusicFX PhotoTable Stk StorageManager Traceur Terminal
+LOCAL_OVERRIDES_PACKAGES := ExactCalculator Calendar CalenderProvider Gallery Gallery2 LatinIME libjni_latinimegoogle DeskClock AEXPapers LiveWallpapersPicker FM2 Music EasterEgg BasicDreams BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicPlayerGO MusicFX PhotoTable Stk StorageManager Traceur Terminal DuckDuckGo WallpaperPickerGoogle BuiltInPrintService
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
