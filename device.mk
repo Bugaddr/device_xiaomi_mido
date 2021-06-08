c70e50d6990a8784506c495f6607fd5f0f7856bc
CHINTU <harrypetersun@outlook.com>
2021-06-08 19:10:44 +0530

    Drop florisboard keyboard seems like default should be best choice
    Dont drop latin ime
    
    Drop florisboard keyboard
    seems like default should be best choice
    
    Signed-off-by: JR CHINTU <harrypetersun@outlook.com>

--- device.mk
+++ device.mk
@@ -430,10 +430,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# FlorisBoard Keyboard
-PRODUCT_PACKAGES += \
-    Florisboard
-
 # Bromite webview
 PRODUCT_PACKAGES += \
 	bromite-webview 
