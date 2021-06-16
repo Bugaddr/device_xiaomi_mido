1b369ca7d9a1f8d8e2ee390ae1671e5b2bde46d0
JR CHINTU <harrypetersun@outlook.com>
2021-06-16 21:32:26 +0000

    Fix typo
    Signed-off-by: JR CHINTU <harrypetersun@outlook.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -7,5 +7,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_OVERRIDES_PACKAGES := LatinIME libjni_latinimegoogle ExactCalculator Calendar CalenderProvider Gallery Gallery2 DeskClock AEXPapers LiveWallpapersPicker FM2 Music EasterEgg BasicDreams BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicPlayerGO MusicFX PhotoTable Stk StorageManager Traceur Terminal DuckDuckGo WallpaperPickerGoogle
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNEDa
+LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
