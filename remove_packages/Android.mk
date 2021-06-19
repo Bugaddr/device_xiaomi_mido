7b3110f56eef1f282456b00c6b5ecbb98f0b224a
CHINTU <harrypetersun@outlook.com>
2021-06-19 20:07:39 +0530

    Remove apps not present in source

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CarrierSetup OBDM_Permissions OemDmTrigger SprintDM SprintHM VZWAPNLib VzwOmaTrigger obdm_stub libjni_latinimegoogle LatinIMEGoogle GalleryGo CalendarGoogle Calendar CalenderProvider Gallery Gallery2 DeskClock LiveWallpapersPicker FM2 Music EasterEgg BasicDreams  BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicFX PhotoTable Stk StorageManager Traceur
+LOCAL_OVERRIDES_PACKAGES := CarrierSetup libjni_latinimegoogle LatinIMEGoogle GalleryGo CalendarGoogle Calendar CalenderProvider DeskClock LiveWallpapersPicker FM2 Music EasterEgg BasicDreams  BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicFX PhotoTable Stk StorageManager Traceur
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
