600cd7a8ed6821c2ccb5ec19813274aae14ece22
CHINTU <harrypetersun@outlook.com>
2021-06-24 19:01:20 +0530

    Refract Debloat apps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := LatinIME libjni_latinimegoogle ExactCalculator Calendar CalenderProvider Gallery Gallery2 DeskClock AEXPapers LiveWallpapersPicker FM2 Music EasterEgg BasicDreams BluetoothMidiService BookmarkProvider Etar WallpaperBackup MusicPlayerGO MusicFX PhotoTable Stk StorageManager Traceur Terminal DuckDuckGo WallpaperPickerGoogle
+LOCAL_OVERRIDES_PACKAGES := libjni_latinimegoogle GalleryGoPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt CalendarProvider CallLogBackup DeskClock LiveWallpapersPicker SimAppDialog WallpaperCropper FM2 EasterEgg BasicDreams BluetoothMidiService BookmarkProvider WallpaperBackup MusicPlayerGO MusicFX PhotoTable Stk StorageManager Traceur Terminal WallpaperPickerGoogle
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
