8cbe9b9313a34f99eeba1214a4b50ee8ca794d4e
CHINTU <harrypetersun@outlook.com>
2021-06-27 22:04:46 +0530

    Revert back to memido default fingerprint
    Hopefully this will fix safety net as corvus os is by default using redfin fingerprint

--- corvus_mido.mk
+++ corvus_mido.mk
@@ -39,14 +39,10 @@ PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ3A.210605.005/7349499:user/release-keys"
-
-# Set BootAnimation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
+BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
