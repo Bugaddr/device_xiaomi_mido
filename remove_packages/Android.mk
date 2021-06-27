72af1de388fc1327f3c843c5021875e1435364b3
CHINTU <harrypetersun@outlook.com>
2021-06-28 01:06:56 +0530

    Drop gboard next time

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := libjni_latinimegoogle CarrierSetup GalleryGoPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt CalendarProvider CallLogBackup DeskClock LiveWallpapersPicker SimAppDialog WallpaperCropper FM2 EasterEgg BasicDreams BluetoothMidiService BookmarkProvider WallpaperBackup StorageManager MusicFX PhotoTable Stk 
+LOCAL_OVERRIDES_PACKAGES := libjni_unbundled_latinimegoogle libjni_latinimegoogle LatinIMEGooglePrebuilt CarrierSetup GalleryGoPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt CalendarProvider CallLogBackup DeskClock LiveWallpapersPicker SimAppDialog WallpaperCropper FM2 EasterEgg BasicDreams BluetoothMidiService BookmarkProvider WallpaperBackup StorageManager MusicFX PhotoTable Stk 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
