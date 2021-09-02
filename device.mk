5df41fc59f9772014f88750182ec70155bc3d0f4
CHINTU <harrypetersun@outlook.com>
2021-09-02 23:59:02 +0530

    Import  Speed profile flags from whyred
    * Source: https://github.com/proton-whyred/android_device_xiaomi_whyred/blob/rvc/device.mk
    * Author: orges,Albert I,others.

--- device.mk
+++ device.mk
@@ -462,3 +462,13 @@ PRODUCT_PACKAGES += \
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
+    
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+USE_DEX2OAT_DEBUG := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+WITH_DEXPREOPT_DEBUG_INFO := false
