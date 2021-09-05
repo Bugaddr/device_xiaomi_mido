a5d289c2fa0287799482c62c643f518c7f6ebe7d
Jr Chintu <harrypetersun@outlook.com>
2021-09-05 16:02:38 +0530

    Drop Snap Camera2
    Signed-off-by: Jr Chintu <harrypetersun@outlook.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := libjni_unbundled_latinimegoogle libjni_latinimegoogle CalculatorGooglePrebuilt LatinIMEGooglePrebuilt CarrierSetup GalleryGoPrebuilt CalendarGooglePrebuilt CallLogBackup LiveWallpapersPicker SimAppDialog WallpaperCropper FM2 EasterEgg BasicDreams BluetoothMidiService BookmarkProvider WallpaperBackup Traceur PhotoTable Stk 
+LOCAL_OVERRIDES_PACKAGES := libjni_unbundled_latinimegoogle libjni_latinimegoogle CalculatorGooglePrebuilt LatinIMEGooglePrebuilt CarrierSetup GalleryGoPrebuilt CalendarGooglePrebuilt CallLogBackup LiveWallpapersPicker SimAppDialog WallpaperCropper FM2 EasterEgg BasicDreams BluetoothMidiService BookmarkProvider WallpaperBackup Traceur PhotoTable Stk Snap Camera2
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
