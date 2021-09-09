8bd942b1d1c2cac3d30bea4ace8ae97837531e05
Jr Chintu <harrypetersun@outlook.com>
2021-09-09 22:56:18 +0530

    Revert "Drop android.hardware.thermal HAL"
    This reverts commit 441c7059255ff4057281ccb4dc36d6bd102775b9.

--- device.mk
+++ device.mk
@@ -403,6 +403,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl:64 \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8953
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
